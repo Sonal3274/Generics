3ab03ed058ea9e51d60e6f4ba68f4d75e19207f3
Sonal Dhage <sonal.dhage@thermondo.de>
2023-03-07 09:40:56 +0100

    Created an example to check whether a type is a subtype

---
+++ src/main/kotlin/IsTypeASubtype.kt
@@ -0,0 +1,20 @@
+open class Animal
+class Cat : Animal()
+
+class Dog : Animal()
+
+fun main() {
+    val cat: Animal = Cat()
+
+    if (cat is Cat) {
+        println("cat is a Cat")
+    }
+
+    if (cat is Animal) {
+        println("cat is an Animal")
+    }
+
+    if (cat !is Dog) {
+        println("cat is not a Dog")
+    }
+}
